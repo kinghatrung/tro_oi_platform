1829d604c3c8c5f92c7ad99c2cdbb3fb94681215
kinghatrung <nguyenred2003@gmail.com>
2026-08-26 05:35:55 +0700

    first commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "API is running 🚀",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
