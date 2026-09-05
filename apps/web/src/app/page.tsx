6dbfef622b68332716d118aac11604f6d47de7d9
kinghatrung <nguyenred2003@gmail.com>
2026-09-05 23:59:53 +0700

    Refactor Home component structure; enhance SearchInput with layout and styling improvements

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,14 +3,16 @@ import { SearchInput } from "@/components/ui";
 
 export default function Home() {
   return (
-    <section
-      className="relative w-full min-h-50 bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/images/bg-trooi.png')" }}
-    >
-      <Header />
-      <div className="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 w-full max-w-150 px-4">
-        <SearchInput />
-      </div>
-    </section>
+    <main>
+      <section
+        className="relative w-full min-h-50 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/images/bg-trooi.png')" }}
+      >
+        <Header />
+        <div className="absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 w-full max-w-220 px-4">
+          <SearchInput />
+        </div>
+      </section>
+    </main>
   );
 }
