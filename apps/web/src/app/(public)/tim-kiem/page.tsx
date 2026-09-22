a265cb95f6b07e6d023da6719d4a118cc158ebf1
kinghatrung <nguyenred2003@gmail.com>
2026-09-22 23:59:23 +0700

    test

--- apps/web/src/app/(public)/tim-kiem/page.tsx
+++ apps/web/src/app/(public)/tim-kiem/page.tsx
@@ -30,6 +30,14 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
         <SearchFilters />
       </Col>
 
+      <Col span={24}>
+        <SearchFilters />
+      </Col>
+
+      <Col span={24}>
+        <SearchFilters />
+      </Col>
+
       <Col lg={18} md={24} xs={24}>
         <SearchResultsList
           items={listings}
